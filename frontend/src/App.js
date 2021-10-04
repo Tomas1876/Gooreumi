ebe9df672e3caa7fc10bbcc208ed5f5389195bd9
Tomas1876 <mnd3259@gmail.com>
2021-10-04 21:01:54 +0900

    에러 uri 처리 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage';
 import RegisterConditionPage from './pages/RegisterConditionPage';
 import Home from './pages/Home';
+import ErrorPage from './pages/ErrorPage';
 import './App.css';
 
 function App() {
